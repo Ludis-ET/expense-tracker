9df9e22fff62dd7538ce7805d1992a0adea9d90f
Ludis <leulsegedmelaku1020@gmail.com>
2024-12-29 19:15:37 +0300

    refactor expenses module; integrate JWT authentication, enhance expense form layout, and improve styling

--- server/src/expenses/expenses.module.ts
+++ server/src/expenses/expenses.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaService } from 'prisma/prisma.service';
+import { ExpensesController } from './expenses.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [ExpensesService],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [ExpensesService, PrismaService],
+  controllers: [ExpensesController],
   exports: [ExpensesService],
 })
 export class ExpensesModule {}
