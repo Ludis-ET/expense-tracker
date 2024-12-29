f7ff5872890d4222ef68970d2b40ed5b27f79feb
Ludis <leulsegedmelaku1020@gmail.com>
2024-12-29 13:37:38 +0300

    implement JWT authentication guard in users controller; restrict access to user data based on user ID

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findAll() {
-    return this.prisma.user.findMany();
+  async findAll(userId: string) {
+    return this.prisma.user.findMany({
+      where: { id: userId },
+    });
   }
 
-  async findOne(id: string) {
-    return this.prisma.user.findUnique({
+  async findOne(id: string, userId: string) {
+    const user = await this.prisma.user.findUnique({
       where: { id },
     });
+
+    if (!user || user.id !== userId) {
+      throw new UnauthorizedException('Access denied');
+    }
+
+    return user;
   }
 }
